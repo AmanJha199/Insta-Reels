3e643c0020b907c056e1235ed82bdeffc1bb3e4e
AmanJha199 <43825635+AmanJha199@users.noreply.github.com>
2021-07-12 00:31:55 +0530

    reels

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/storage'
+import 'firebase/firestore';
+
+firebase.initializeApp(
+    {
+        apiKey: "AIzaSyBwV4sS0cLgYYqFCcBfCHkC-dZAk66YQU4",
+        authDomain: "insta-reels-46aec.firebaseapp.com",
+        projectId: "insta-reels-46aec",
+        storageBucket: "insta-reels-46aec.appspot.com",
+        messagingSenderId: "165421465486",
+        appId: "1:165421465486:web:29ea9695899a15de98aedf",
+        measurementId: "G-55WY8H9ECT"
+    })
+
+export const auth = firebase.auth();
+const firestore = firebase.firestore();
+export const database = {
+    users: firestore.collection('users'),
+    getCurrentTimeStamp: firebase.firestore.FieldValue.serverTimestamp
+}
+export const storage = firebase.storage();
