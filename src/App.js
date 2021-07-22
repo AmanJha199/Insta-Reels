a73eea2bb621fe3ece679cd6689d1f682bba330a
AmanJha199 <43825635+AmanJha199@users.noreply.github.com>
2021-07-23 00:08:35 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Signup from './Components/Signup'
+import AuthProvider from './Context/AuthProvider'
+import POC1 from './Material-UI/POC1'
 
 function App() {
   return (
-    <h1>Hello</h1>
+    // <AuthProvider>
+    // <Signup></Signup>
+    // </AuthProvider>
+    <POC1></POC1>
   );
 }
 
---
+++ src/Material-UI/POC1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function POC1() {
+    return (
+        <div>
+            <h1>Hello let's learn material-ui</h1>
+        </div>
+    )
+}
+
+export default POC1
